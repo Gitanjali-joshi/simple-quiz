76a9ae46187c4a3ede120f2815dce9f4777a80e5
Gitanjali-joshi <geetujoshi777@gmail.com>
2022-02-08 17:26:26 +0530

    first commit

---
+++ qui.js
@@ -0,0 +1,101 @@
+const quizData = [
+    {
+        question: "Artificial Intelligence is about_____",
+        a: "Playing a game on Computer",
+        b: "Making a machine Intelligent",
+        c: "Programming on Machine with your Own Intelligence",
+        d: "Putting your intelligence in Machine",
+        correct: "b",
+    },
+    {
+        question: "Who is known as the -Father of AI",
+        a: "Fisher Ada",
+        b: "Alan Turing",
+        c: "John McCarthy",
+        d: "Allen Newell",
+        correct: "c",
+    },
+    {
+        question: "What does HTML stand for?",
+        a: "Hypertext Markup Language",
+        b: "Hypertext Markdown Language",
+        c: "Hyperloop Machine Language",
+        d: "Helicopters Terminals Motorboats Lamborginis",
+        correct: "a",
+    },
+    {
+        question: "The application/applications of Artificial Intelligence is/are",
+        a: "Expert Systems",
+        b: "Gaming",
+        c: "Vision Systems",
+        d: "All of the above",
+        correct: "d",
+    },
+
+
+];
+
+const quiz= document.getElementById('quiz')
+const answerEls = document.querySelectorAll('.answer')
+const questionEl = document.getElementById('question')
+const a_text = document.getElementById('a_text')
+const b_text = document.getElementById('b_text')
+const c_text = document.getElementById('c_text')
+const d_text = document.getElementById('d_text')
+const submitBtn = document.getElementById('submit')
+
+
+let currentQuiz = 0
+let score = 0
+
+loadQuiz()
+
+function loadQuiz() {
+
+    deselectAnswers()
+
+    const currentQuizData = quizData[currentQuiz]
+
+    questionEl.innerText = currentQuizData.question
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+    c_text.innerText = currentQuizData.c
+    d_text.innerText = currentQuizData.d
+}
+
+function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false)
+}
+
+function getSelected() {
+    let answer
+    answerEls.forEach(answerEl => {
+        if(answerEl.checked) {
+            answer = answerEl.id
+        }
+    })
+    return answer
+}
+
+
+submitBtn.addEventListener('click', () => {
+    const answer = getSelected()
+    if(answer) {
+       if(answer === quizData[currentQuiz].correct) {
+           score++
+       }
+
+       currentQuiz++
+
+       if(currentQuiz < quizData.length) {
+           loadQuiz()
+       } else {
+           quiz.innerHTML = `
+           <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+
+           <button onclick="location.reload()">Reload</button>
+           `
+       }
+    }
+})
+
